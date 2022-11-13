97967fc641103911e43705454dabd04e95ff2dbb
ParthSonani <parthsonani08@gmail.com>
2022-11-13 18:54:48 -0500

    Terminal UI to interact with data

--- terminalui.py
+++ terminalui.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+import pandas as pd
 
 def convertTuple(tup):
     str = ''.join(tup)
@@ -16,6 +17,71 @@ def sql_fetch(con):
         print(j)
 
     return table_list
+
+def display_operations():
+    print("\nChoose operation to perform on " + table_name + " :\n 1. Update\n 2. Delete \n 3. Download table (.csv)\n"
+                                                             " 4. Enter to 0 to exit \n")
+
+def update_data(col):
+    update_id = input("Enter " + col_name[0] + " of the record to be updated : ")
+    id = col
+    try:
+        # print(table_name)
+        rec = cursor.execute(('SELECT * FROM ' + table_name + ' WHERE "{}"=?'.format(id.replace('"', '""'))),
+                             (update_id,))
+        rec = rec.fetchone()
+        print(rec)
+        print("\nChoose record to update: ")
+        for cols in col_name[1:]:
+            print(cols)
+
+        up_no = input("\n Input here: ")
+
+        for cols in col_name[1:]:
+            if (up_no == cols):
+                up_rec = input("\n Enter the updated value : ")
+                rec1 = cursor.execute('UPDATE ' + table_name + ' SET ' + cols + '=? WHERE ' + id + '=' + update_id + '',
+                                      (up_rec,))
+                rec1 = rec1.rowcount
+                print("\nNumber of rows affected: " + str(rec1))
+                conn.commit()
+                print("Record updated successfully !!!\n")
+                rec2 = cursor.execute(('SELECT * FROM ' + table_name + ' WHERE "{}"=?'.format(id.replace('"', '""'))),
+                                      (update_id,))
+                rec2 = rec2.fetchone()
+                print(rec2)
+
+
+    except Exception as e:
+        print(e)
+
+def delete_data(col):
+    id = col
+    delete_id = input("Enter " + col_name[0] + " of the record to be deleted : ")
+    try:
+
+        rec = cursor.execute(('SELECT * FROM ' + table_name + ' WHERE "{}"=?'.format(id.replace('"', '""'))),
+                             (delete_id,))
+        rec = rec.fetchone()
+        print(rec)
+        ans = input("\nAre you sure you want to delete this record? (Y/n): ")
+
+        if (ans == 'Y' or ans == 'y'):
+            rec1 = cursor.execute('DELETE from ' + table_name + ' WHERE ' + id + '=' + delete_id + '')
+            rec1 = rec1.rowcount
+            print("\nNumber of rows affected: " + str(rec1))
+            conn.commit()
+            print("Record deleted successfully !!!\n")
+
+    except Exception as e:
+        print(e)
+
+def download_data():
+    db_df = pd.read_sql_query('SELECT * FROM ' + table_name + '', conn)
+    db_df.to_csv('' + table_name + '.csv', index=False)
+    print("Records for table " + table_name + " downloaded successfully !!!")
+
+
 
 # Database connection
 print("Connecting to local database ...")
@@ -42,39 +108,26 @@ for i in tables:
             #print(row.keys(0), row.keys(1), row.keys(2), row.keys(3), row.keys(4))
             print(str(row[0]),'  |  ', str(row[1]), '|', str(row[2]), '|', str(row[3]), '|', str(row[4]))
 
-        print("\nChoose operation to perform on "+ table_name+" :\n 1. Update\n 2. Delete \n 3. Download table (.csv)\n"
-                                                                 " 4. Enter to 0 to exit \n")
+        display_operations()
         op = input("Input here: ")
+
+        # Update query
         if(op == "1" or op == "update" or op == "Update"):
             #print("hahahah")
-            update_id = input("Enter "+col_name[0]+ " of the record to be updated : ")
+            #update_id = input("Enter "+col_name[0]+ " of the record to be updated : ")
             id = col_name[0]
+            update_data(id)
 
+        # Delete query
+        if (op == "2" or op == "delete" or op == "Delete"):
+            #delete_id = input("Enter " + col_name[0] + " of the record to be deleted : ")
+            id = col_name[0]
+            delete_data(id)
 
-            try:
-                #print(table_name)
-                rec = cursor.execute(('SELECT * FROM '+table_name+' WHERE "{}"=?'.format(id.replace('"', '""'))), (update_id,) )
-                rec = rec.fetchone()
-                print(rec)
-                print("\nChoose record to update: ")
-                for cols in col_name[1:]:
-                    print(cols)
+        # Download data
+        if (op == "3" or op == "download" or op == "Download table"):
+            download_data()
 
-                up_no = input("\n Input here: ")
-
-                for cols in col_name[1:]:
-                    if(up_no == cols):
-                        up_rec = input("\n Enter the updated value : ")
-                        rec1 = cursor.execute('UPDATE '+table_name+' SET '+cols+'=? WHERE '+id+'='+update_id+'', (up_rec,))
-                        rec1 = rec1.rowcount
-                        print("\nNumber of rows affected: " +str(rec1))
-                        conn.commit()
-                        print("Record updated successfully !!!\n")
-                        rec2 = cursor.execute(('SELECT * FROM '+table_name+' WHERE "{}"=?'.format(id.replace('"', '""'))), (update_id,))
-                        rec2 = rec2.fetchone()
-                        print(rec2)
-
-
-            except Exception as e:
-                print(e)
-
+        # Exit application
+        if (op == "0" or op == "4" or op == "exit"):
+            exit()
