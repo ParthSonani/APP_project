0ef3a97ed50f74516ee7b7c945775bdba2fb7a5d
ParthSonani <parthsonani08@gmail.com>
2022-11-14 17:25:37 -0500

    Terminal GUI that interacts with TDG

--- terminalui.py
+++ terminalui.py
@@ -1,140 +1,65 @@
 import sqlite3
 import json
+from connection1 import Connection
 import pandas as pd
+from TDG import TableDataGateway
 
-def convertTuple(tup):
-    str = ''.join(tup)
-    return str
+TableDataGateway = TableDataGateway()
 
-def sql_fetch(con):
-    cursorObj = con.cursor()
-    cursorObj.execute('SELECT name from sqlite_master where type= "table"')
-    table_l = cursorObj.fetchall()
-    table_list = []
-    for i in table_l:
-        j = convertTuple(i)
-        table_list.append(j)
-        print(j)
+def table_creation():
+    f = open('data.json')
+    data = json.load(f)
+    print(data)
+    TableDataGateway.create_tables()
+    TableDataGateway.insert_data_departure(data)
+    TableDataGateway.insert_data_arrival(data)
+    TableDataGateway.insert_data_flight_detail(data)
 
-    return table_list
+def display_operations(table_n):
+    table_name = table_n
+    print("\nChoose operation to perform on " + table_name + " :\n 1. Update\n 2. Delete \n 3. Download table (.csv)\n"
+                                                             " 4. Enter to 0 to exit \n")
 
-def display_tables(tb_name):
-    table_name = tb_name
+def get_input():
+    table_name = input("\n\nEnter the name of table you wish to access (Press 0 to exit): ")
 
-    if (table_name == i):
-        cursor.execute(('SELECT * FROM "{}" '.format(table_name.replace('"', '""'))))
-        tab = cursor.fetchall()
-        col_name = [i[0] for i in cursor.description]
-        col_len = len(col_name)
-        # for a in col_name:
-        print("--------------------------------------------------------------------")
-        print(*col_name, sep=" | ")
-        print("--------------------------------------------------------------------")
-        stmt = []
-        for row in tab:
-            print(row)
-            # print(str(row[0]),'  |  ', str(row[1]), '|', str(row[2]), '|', str(row[3]), '|', str(row[4]))
+    return table_name
 
-        display_operations()
+table_creation()
+
+while True:
+    tables = TableDataGateway.sql_fetch()
+    table_name = get_input()
+
+    if (table_name in tables):
+
+        # Display table and return list of column names
+        col_name = TableDataGateway.display_tables(table_name)
+
+        display_operations(table_name)
         op = input("Input here: ")
 
         # Update query
         if (op == "1" or op == "update" or op == "Update"):
             # print("hahahah")
             # update_id = input("Enter "+col_name[0]+ " of the record to be updated : ")
-            id = col_name[0]
-            update_data(id, col_name)
+            #id = col_name[0]
+            TableDataGateway.update_data(col_name, table_name)
 
         # Delete query
         if (op == "2" or op == "delete" or op == "Delete"):
             # delete_id = input("Enter " + col_name[0] + " of the record to be deleted : ")
-            id = col_name[0]
-            delete_data(id, col_name)
+            #id = col_name[0]
+            TableDataGateway.delete_data(col_name, table_name)
 
         # Download data
         if (op == "3" or op == "download" or op == "Download table"):
-            download_data()
+            TableDataGateway.download_data(table_name)
 
         # Exit application
         if (op == "0" or op == "4" or op == "exit"):
             exit()
 
+    else :
+        exit()
 
-def display_operations():
-    print("\nChoose operation to perform on " + table_name + " :\n 1. Update\n 2. Delete \n 3. Download table (.csv)\n"
-                                                             " 4. Enter to 0 to exit \n")
-
-def update_data(col, col_name):
-    update_id = input("Enter " + col_name[0] + " of the record to be updated : ")
-    id = col
-    try:
-        # print(table_name)
-        rec = cursor.execute(('SELECT * FROM ' + table_name + ' WHERE "{}"=?'.format(id.replace('"', '""'))),
-                             (update_id,))
-        rec = rec.fetchone()
-        print(rec)
-        print("\nChoose record to update: ")
-        for cols in col_name[1:]:
-            print(cols)
-
-        up_no = input("\n Input here: ")
-
-        for cols in col_name[1:]:
-            if (up_no == cols):
-                up_rec = input("\n Enter the updated value : ")
-                rec1 = cursor.execute('UPDATE ' + table_name + ' SET ' + cols + '=? WHERE ' + id + '=' + update_id + '',
-                                      (up_rec,))
-                rec1 = rec1.rowcount
-                print("\nNumber of rows affected: " + str(rec1))
-                conn.commit()
-                print("Record updated successfully !!!\n")
-                rec2 = cursor.execute(('SELECT * FROM ' + table_name + ' WHERE "{}"=?'.format(id.replace('"', '""'))),
-                                      (update_id,))
-                rec2 = rec2.fetchone()
-                print(rec2)
-
-
-    except Exception as e:
-        print(e)
-
-def delete_data(col, col_name):
-    id = col
-    delete_id = input("Enter " + col_name[0] + " of the record to be deleted : ")
-    try:
-
-        rec = cursor.execute(('SELECT * FROM ' + table_name + ' WHERE "{}"=?'.format(id.replace('"', '""'))),
-                             (delete_id,))
-        rec = rec.fetchone()
-        print(rec)
-        ans = input("\nAre you sure you want to delete this record? (Y/n): ")
-
-        if (ans == 'Y' or ans == 'y'):
-            rec1 = cursor.execute('DELETE from ' + table_name + ' WHERE ' + id + '=' + delete_id + '')
-            rec1 = rec1.rowcount
-            print("\nNumber of rows affected: " + str(rec1))
-            conn.commit()
-            print("Record deleted successfully !!!\n")
-
-    except Exception as e:
-        print(e)
-
-def download_data():
-    db_df = pd.read_sql_query('SELECT * FROM ' + table_name + '', conn)
-    db_df.to_csv('' + table_name + '.csv', index=False)
-    print("Records for table " + table_name + " downloaded successfully !!!")
-
-
-
-# Database connection
-print("Connecting to local database ...")
-conn = sqlite3.connect('flight.db')
-cursor = conn.cursor()
-print("Connected to flight database !!")
-print("List of tables:-")
-
-tables = sql_fetch(conn)
-
-table_name = input("Enter the name of table you wish to access: ")
-
-for i in tables:
-    display_tables(table_name)
