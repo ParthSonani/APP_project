8cdc1c6377ae99397a848d2e963307bd318ac206
ParthSonani <parthsonani08@gmail.com>
2022-11-14 19:40:26 -0500

    ReadMe

--- mainprog.py
+++ mainprog.py
@@ -1,21 +1,30 @@
 import requests
 import json
 
-params = {
-  'access_key': '7838e177f2ab9fddc1dbf077cb72708e'
-}
+class ApiCall:
+  def acall(self):
+    params = {
+      'access_key': '7838e177f2ab9fddc1dbf077cb72708e'
+    }
 
-api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
+    api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
 
-api_response = api_result.json()
-api_data = json.dumps(api_response)
-new_data = json.loads(api_data)
-get_data = new_data['data']
-print(get_data)
+    api_response = api_result.json()
+    api_data = json.dumps(api_response)
+    new_data = json.loads(api_data)
+    get_data = new_data['data']
+    print(get_data)
+
+    return get_data
+
+  def download_api_data(self, get_data):
+    # Downloading json data into a file
+    data = json.dumps(get_data)
+    file = open("data.json", "w")
+    file.write(data)
+    print("API data successfully downloaded!!")
 
 
-# Downloading json data into a file
-data = json.dumps(get_data)
-file = open("data.json","w")
-file.write(data)
-print("API data successfully downloaded!!")
+api = ApiCall()
+get_data = api.acall()
+api.download_api_data(get_data)
