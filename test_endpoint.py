dd99979cb24fc925a16d091cb9e82629d2e78125
ParthSonani <parthsonani08@gmail.com>
2022-11-14 21:52:45 -0500

    Testing

--- test_endpoint.py
+++ test_endpoint.py
@@ -1,14 +1,20 @@
 import requests
+import unittest
+from connection import Connection
 
 Endpoint = "http://api.aviationstack.com/v1/flights?access_key=7838e177f2ab9fddc1dbf077cb72708e"
 
+
+class TestApp(unittest.TestCase):
+    def test_get_connection(self):
+        self.assertEqual(Connection.get_connection(), Connection.conn)
+
 #Testing function
-def test_endpoint():
-    response = requests.get(Endpoint)
-    print(response)
-    if response.status_code != 200:
-        print("wrong status code" + response.status_code)
-    assert response.status_code == 200
+    def test_endpoint(self):
+        response = requests.get(Endpoint)
+        print(response)
+        if response.status_code != 200:
+            print("wrong status code" + response.status_code)
+        assert response.status_code == 200
 
 
-test_endpoint()
